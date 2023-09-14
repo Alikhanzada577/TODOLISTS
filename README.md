9edffda622208f861f04c47f5f08c593e328ca83
Muhammad Ali <muhammadali94200@gmail.com>
2023-09-14 14:08:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TodoList-Using-NextJs
+Our project is a Todo list web app built with Next.js and styled using Tailwind CSS. It allows users to add and delete tasks for efficient task management.
---
+++ app/page.js
@@ -0,0 +1,75 @@
+'use client'
+import React, { useState } from 'react'
+
+const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
+  const [mainTask, setmainTask] = useState([])
+
+  const submitHandler = (e)=>{
+  e.preventDefault()
+  setmainTask([...mainTask,{title,desc}])
+
+  settitle("")
+  setdesc("") 
+  }
+  const deleteHandler =(i)=>{
+  let copyTask = [...mainTask]
+  copyTask.splice(i,1)
+  setmainTask(copyTask)
+  }
+  let renderTask = <h2>No Task Available</h2>
+ if(mainTask.length>0){
+  renderTask = mainTask.map((t,i)=>{
+    return (
+     <li key={i} className=' flex items-center justify-between  mb-5'>
+      <div className='flex justify-between w-2/3 items-center'>
+     <h5 className='font-semibold text-2xl'>{t.title}</h5>
+     <h5 className='font-semibold text-xl'>{t.desc}</h5>
+     </div>
+     <button 
+     onClick={()=>{
+      deleteHandler(i)
+     }}
+     className='bg-red-400 text-white px-4 py-2 rounded font-bold '>
+      Delete
+      </button>
+     </li>
+    );
+  });
+ }
+
+  return (
+    <>
+      <h1 className='bg-black text-white text-center p-5 text-2xl text-bold '> Todo List</h1>
+      <form onSubmit={submitHandler}>
+        <input type='text'
+         className='text-xl border-zinc-800 border-2 m-5 px-4 py-2'
+         placeholder='Enter Task Here '
+         value={title}
+         onChange={(e)=>{
+           settitle(e.target.value)
+         }}
+         />
+          <input type='text'
+         className='text-xl border-zinc-800 border-2 m-5 px-4 py-2'
+         placeholder='Enter Description Here '
+         value={desc}
+         onChange={(e)=>{
+          setdesc(e.target.value)
+        }}
+         />
+         <button className='bg-black text-white px-4 py-3 text-2xl font-bold rounded m-5 '>Add Task</button>
+      </form>
+      <hr/>
+      <div className='p-8 bg-slate-300'>
+        <ul>
+          {renderTask}
+        </ul>
+      </div>
+      
+    </>
+  )
+}
+
+export default page
